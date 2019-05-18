bf78159b50db073becc5cd19161b6159db46ee9a
sketchbook21 <gilbert.hsu2@gmail.com>
2019-05-18 11:27:27 -0400

    conflict

--- app/javascript/react/containers/SearchResultsContainer.js
+++ app/javascript/react/containers/SearchResultsContainer.js
@@ -11,11 +11,41 @@ class SearchResultsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      newDetailData: {},
+      similarProductsData: [],
     }
   }
 
+  fetchResults = (payload) => {
+    fetch(`/api/v1/search/`, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      credentials: 'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`, error = new Error(errorMessage)
+        throw (error)
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({
+        newDetailData: body.amazon_response_first,
+        similarProductsData: body.amazon_response_similar
+      })
+      console.log(body)
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <SearchBar />
