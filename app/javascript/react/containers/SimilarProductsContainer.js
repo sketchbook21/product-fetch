3acbf2aa8f079954a71ffcd7f8649d229ac32c95
sketchbook21 <gilbert.hsu2@gmail.com>
2019-05-23 23:07:30 -0400

    wip

--- app/javascript/react/containers/SimilarProductsContainer.js
+++ app/javascript/react/containers/SimilarProductsContainer.js
@@ -9,9 +9,30 @@ class SimilarProductsContainer extends Component {
   }
 
   render() {
+    let products = this.props.data
+    let productsDisplay = products.map(product => {
+      let productPrice = product.ItemAttributes["ListPrice"] ? product.ItemAttributes["ListPrice"]["FormattedPrice"] : "N/A"
+      debugger
+      return(
+        <div className="result-tile">
+          <div className="row underline">
+            <div className="small-4 columns center">
+              <a href={product.DetailPageURL} target="_blank"><img src={product.LargeImage.URL} className="result-image-tile" /></a>
+            </div>
+            <div className="small-9 columns">
+              <a className="s4 w3" href={product.DetailPageURL} target="_blank">{product["ItemAttributes"]["Title"]}</a>
+            </div>
+            <div className="small-3 columns right">
+              <a className="s4 w7" href={product.DetailPageURL} target="_blank">{productPrice}</a>
+            </div>
+          </div>
+        </div>
+      )
+    })
     return (
-      <div className="result-detail ui-block">
-        Hello from SimilarProductsContainer
+      <div className="result-container">
+        <h5>Similar Products</h5>
+        {productsDisplay}
       </div>
     )
   }
